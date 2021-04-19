2f0fa5a5de818b5bf61598f7512d8132848e3a9d
bilgi atahan <bilgi_atahan@hotmail.com>
2021-04-20 01:25:31 +0300

    login and register has move

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const bookAdd = require('./routers/book-add')
 const bookdelete = require('./routers/book-delete')
 const bookupdate = require('./routers/book-update')
+const login = require('./routers/login')
+const register = require('./routers/register')
 const Users = require('./models/users')
 const users = new Users();
 
@@ -16,60 +18,11 @@ app.get('/', (req, res) => {
     res.redirect('/login')
 });
 
+app.use(login)
+app.use(register)
 app.use(bookAdd)
 app.use(bookdelete)
 app.use(bookupdate)
-
-app.get('/login',(req, res) => {
-    res.render('login')
-});
-app.post('/login', authCheck, (req, res) => {
-    res.redirect('/' + req.body.id)
-})
-app.get('/:id',userCheck, (req, res) => {
-    const user = users.getUser(req.params.id)
-    const bookOfUser = users.getBooks(req.params.id)
-    res.render('index', {
-        user: user,
-        books : bookOfUser
-    })
-})
-app.get('/register', (req, res) => {
-    res.render('register')
-})
-
-app.post('/register', (req, res) => {
-    users.openAccount(req.body)
-    res.redirect('/login')
-})
-
-// app.get('/:id', (req, res) => {
-//     const { id } = req.params
-//     if(id){
-//         const book_details = booksDirectory.find(b => b.isbn === id)
-//         res.render("book-details", { bookDetails: book_details })
-//     }
-// })
-function authCheck(req, res, next) {
-    const username = req.body.username
-    const user = users.checkUser(username)
-    if (user)
-        if (user.password === req.body.password) {
-            req.body = user
-            next()
-            return
-        }
-        else
-            res.send('404')
-}
-function userCheck(req,res,next){
-    const id = req.params.id
-    if(users.getUser(id)){
-        res.redirect('/login')
-    }
-    else
-        res.send('couldnt find')
-}
 
 app.listen(3000)
 
