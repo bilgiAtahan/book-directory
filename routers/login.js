050d17cb3171dc96295159ad8f8e1ced3419ef60
bilgi atahan <bilgi_atahan@hotmail.com>
2021-05-03 22:00:33 +0300

    added auth

--- routers/login.js
+++ routers/login.js
@@ -1,32 +1,17 @@
 const express = require('express')
 const router = express.Router();
-const user = require('./user')
-const Users = require('../models/users')
-const users = new Users();
+const user = require('../models/users');
 
-router.get('/login', (req, res) => {
+router.get('/', (req, res) => {
     res.render('login')
 });
 
-router.use(user)
+router.post('/', authenticate)
 
-router.post('/login', authCheck, (req, res) => {
-    res.redirect('/user/' + req.body.id)
-})
-
-
-function authCheck(req, res, next) {
-    const username = req.body.username
-    const user = users.checkUser(username)
-    if (user) {
-        if (user.password === req.body.password) {
-            req.body = user
-            next()
-            return
-        } else
-            res.send('404')
-    } else
-        res.send('Kullancı bulunamadı')
-
-}
 module.exports = router;
+
+function authenticate(req, res, next) {
+    user.authenticate(req.body)
+        .then(user => user ? res.redirect("/user/" + user.id) : res.status(400).json({ message: 'Username or password is incorrect' }))
+        .catch(err => next(err));
+}
